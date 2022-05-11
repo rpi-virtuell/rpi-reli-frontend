913315aba099c32a932c4b1774bb6808eddcc55c
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2022-05-11 08:49:02 +0200

    INITIAL COMMIT: ADDED new svgs to illustrate taxonomies ADDED structure to display material and search filters on a webpage

--- facetwp_templates/material_suchausgabe.php
+++ facetwp_templates/material_suchausgabe.php
@@ -1,56 +1,56 @@
 <div class="material-grid">
     <?php
-    while (have_posts())
+    while (have_posts()) :
         the_post();
-    ob_start();
-    ?>
-    <article class="entry-card">
-        <div class="facet-treffer">
-            <div class="einrichtung">
-
-            </div>
-            <div class="material-title">
-                <h2>
-                    <a href="<?php the_permalink(); ?>"> <?php the_title() ?> </a>
-                </h2>
-            </div>
-            <div class="picture">
-                <img src="<?php the_post_thumbnail_url(); ?>" alt="">
-            </div>
-            <div class="excerpt"></div>
-            <div class="taxonomien">
-
-                <div class="author">
-
+        ob_start();
+        ?>
+        <article class="entry-card">
+            <div class="facet-treffer">
+                <div class="material-title">
+                    <h2>
+                        <a href="<?php the_permalink(); ?>"> <?php the_title() ?> </a>
+                    </h2>
                 </div>
-                <div class="altergruppe">
+                <?php if (!empty(get_the_post_thumbnail_url())) { ?>
+                    <a class="material-picture boundless-image" href=" <?php the_permalink(); ?>">
+                        <img src="<?php the_post_thumbnail_url(); ?>" alt="">
+                        <span class="ct-ratio" style="padding-bottom: 75%"></span>
+                    </a>
+                <?php } ?>
+                <div class="excerpt"></div>
+                <div class="ct-ghost"></div>
+                <div style="clear: both"></div>
+                <div class="taxonomien">
+                    <div class="author">
+                        <img class="taxonomy-icon"
+                             src="<?php echo __RPI_RELI_FRONTEND_URI__ . "assets/author.svg" ?>"
+                             alt="">
+                        <a href="<?php echo __RPI_RELI_FRONTEND_SEARCH_URL__ . '?_search=' . get_the_author() ?>"> <?php the_author() ?> </a>
+                    </div>
 
+                    <?php
+                    $taxonomies = get_object_taxonomies('materialien', 'objects');
+                    foreach ($taxonomies as $taxonomy) { ?>
+                        <?php $terms = RpiReliFrontendSearch::get_taxonomy_to_html(get_the_ID(), $taxonomy->name);
+                        if (!empty($terms)) {
+                            ?>
+                            <div class="<?php echo $taxonomy->name ?>">
+                                <img class="taxonomy-icon"
+                                     src="<?php echo __RPI_RELI_FRONTEND_URI__ . "assets/" . $taxonomy->name . ".svg" ?>"
+                                     alt="">
+                                <?php echo $terms ?>
+                            </div>
+                            <?php
+                        }
+                    }
+                    ?>
                 </div>
-                <div class="materialtyp">
+        </article>
 
-                </div>
-                <div class="anlaesse">
-
-                </div>
-                <div class="kinderaktivitaet"></div>
-
-                <div class="kinderfahrung">
-
-                </div>
-                <div class="schlagworte">
-
-                </div>
-                <div class="religioese-geschichten-erleben">
-
-                </div>
-                <div class="lizenzen">
-
-                </div>
-            </div>
-    </article>
-
-    <?php
-    $buffer = ob_get_contents();
-    ob_end_clean();
+        <?php
+        $buffer = ob_get_contents();
+        ob_end_clean();
+        echo $buffer;
+    endwhile;
     ?>
 </div>
--- search.php
+++ search.php
@@ -13,43 +13,71 @@ class RpiReliFrontendSearch
 {
     function __construct()
     {
-        add_shortcode('rpi-reli-fronend-search', array($this, 'search'));
+        add_action('wp_enqueue_scripts', function () {
+            wp_enqueue_style('rpi_reli_frontend_search_style', plugin_dir_url(__FILE__) . 'search.css');
+        });
+        add_shortcode('rpi-reli-frontend-search', array($this, 'search'));
+        define('__RPI_RELI_FRONTEND_DIR__', plugin_dir_path(__FILE__));
+        define('__RPI_RELI_FRONTEND_URI__', plugin_dir_url(__FILE__));
     }
 
     function search()
     {
-//        ob_start();
-//        load_template(dirname(__FILE__) . '/templates/single-termin-block.php', false);
-//       return ob_get_clean();
+        ob_start();
+        define('__RPI_RELI_FRONTEND_SEARCH_URL__', site_url());
 
         //TODO: fill search-filters automatically ? via option page ? -> select which facet should be displayed in search
         //  NOTE: Find way to get all FACETS of facetwp
 
-        wp_enqueue_style('rpi_reli_frontend_search_style', get_stylesheet_directory_uri() . '/search.css');
         ?>
         <div class="search-page-grid">
             <div class="search-bar">
                 <?php echo facetwp_display('facet', 'search'); ?>
-                <input type="button" value="Suche">
-                <button> Erweiterte Suche</button>
+                <button class="wp-block-search__button" name="filter-button"> Erweiterte Suche</button>
             </div>
             <div class="search-filters">
-                <?php echo facetwp_display('facet', 'materialtypen'); ?>
-                <?php echo facetwp_display('facet', 'anlaesse'); ?>
-                <?php echo facetwp_display('facet', 'altergruppe'); ?>
-                <?php echo facetwp_display('facet', 'kinderaktivitaeten'); ?>
-                <?php echo facetwp_display('facet', 'Einrichtungen'); ?>
-                <?php echo facetwp_display('facet', 'Religiöse Geschichten erleben'); ?>
-                <?php echo facetwp_display('facet', 'Schlagworte'); ?>
-                <?php echo facetwp_display('facet', 'Lizenzen'); ?>
-                <?php echo facetwp_display('facet', 'Sortieren'); ?>
+
+                <?php
+                $taxonomies = get_object_taxonomies('materialien', 'objects');
+                foreach ($taxonomies as $taxonomy) {
+                    if (is_a($taxonomy, 'WP_Taxonomy')) {
+                        ?>
+                        <div class="single-filter">
+                            <h4>
+                                <img class="filter-icon"
+                                     src="<?php echo __RPI_RELI_FRONTEND_URI__ . "assets/" . $taxonomy->name . ".svg" ?>"
+                                     alt="">
+                                <?php echo $taxonomy->label ?>
+                            </h4>
+                            <?php echo facetwp_display('facet', $taxonomy->name); ?>
+                        </div>
+                        <?php
+                    }
+                }
+                ?>
             </div>
             <div class="search-results">
-
                 <?php echo facetwp_display('template', 'material_suchausgabe'); ?>
             </div>
         </div>
         <?php
+        return ob_get_clean();
+    }
+
+    static function get_taxonomy_to_html($postID, $taxonomy_name, $search_URL = __RPI_RELI_FRONTEND_SEARCH_URL__)
+    {
+        $taxonomies = '';
+        if (is_array($results = wp_get_post_terms($postID, $taxonomy_name))) {
+            $lastkey = array_key_last($results);
+            foreach ($results as $key => $result) {
+                if (is_a($result, 'WP_Term')) {
+                    $taxonomies .= '<a href="' . $search_URL . '?_' . $taxonomy_name . '=' . $result->slug . '">' . $result->slug . '</a>';
+                    if (count($results) > 1 && $key != $lastkey)
+                    $taxonomies .= ', ';
+                }
+            }
+        }
+        return $taxonomies;
     }
 }
 
