51f3a9a9e075a893580c87eb554af75348d40c79
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2022-10-12 14:50:40 +0200

    CHANGES: ADDED new tags to taxonomy filtered archives to show the posttype better UPDATED CSS rules of the material-report

--- search.php
+++ search.php
@@ -97,6 +97,27 @@ class RpiReliFrontendSearch
                 <?php
             }
         });
+
+        add_action('blocksy:loop:card:start', function (){
+            if (get_post_type() === 'organisation'){
+                ?>
+                <div class="card-tag-bar">
+                <div title="Organisation" class="card-tag organisation-tag">
+                    <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><rect fill="none" height="24" width="24"/><path d="M12,7V3H2v18h20V7H12z M10,19H4v-2h6V19z M10,15H4v-2h6V15z M10,11H4V9h6V11z M10,7H4V5h6V7z M20,19h-8V9h8V19z M18,11h-4v2 h4V11z M18,15h-4v2h4V15z"/></svg>
+                </div>
+                </div>
+                <?php
+            }
+            if (get_post_type() === 'fortbildung'){
+                ?>
+                <div class="card-tag-bar">
+                <div title="Fortbildung" class="card-tag fortbildung-tag">
+                    <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24"/></g><g><g><path d="M20,8h-3V6c0-1.1-0.9-2-2-2H9C7.9,4,7,4.9,7,6v2H4c-1.1,0-2,0.9-2,2v10h20V10C22,8.9,21.1,8,20,8z M9,6h6v2H9V6z M20,18H4 v-3h2v1h2v-1h8v1h2v-1h2V18z M18,13v-1h-2v1H8v-1H6v1H4v-3h3h10h3v3H18z"/></g></g></svg>
+                </div>
+                </div>
+                <?php
+            }
+        });
     }
 
     static function getSearchPage()
