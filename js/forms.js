f5aa15cf8078ac5ac6280c225275091e8d664e2e
Joachim Happel <joachim.happel@gmail.com>
2022-10-17 19:01:35 +0200

    formstyling

--- js/forms.js
+++ js/forms.js
@@ -6,23 +6,18 @@ jQuery(document).ready($=>{
         $(this).nextAll().wrapAll('<div id="wrap"></div>');
     });
 
+
+
     $('details').attr('open','').find('#wrap').css('display','none');
     $('details.open div#wrap').css('display','block');
     $('details summary').click(function(e) {
         e.preventDefault();
         $(this).siblings('div#wrap').slideToggle(function(){
+            console.log('toggle');
             $(this).parent('details').toggleClass('open');
+            $('article.fortbildung').toggle();
+            $('article.organisation').toggle();
         });
     });
 
-    $=jQuery;
-    $('.organisation-edit-section summary.button').click(e=>{
-        if($('.organisation-edit-section[open]').length>0){
-             $('article.organisation').slideDown();
-
-        }else{
-            $('article.organisation').slideUp();
-
-        }
-    });
 });
