87703ce58d70856a53b47e010629614e370bd782
Joachim Happel <joachim.happel@gmail.com>
2022-10-31 11:34:32 +0100

    fix Join Buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
 jQuery(document).ready($=>{
     $('article.fortbildung').css({'min-height':$('.reli-sidebar').height()+'px'});
+    $('article.author').css({'min-height':$('.reli-sidebar').height()+'px'});
 });
